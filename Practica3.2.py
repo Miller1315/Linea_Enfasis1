9ed150f1d8e6d179bd28757d05b49a0fe02fff0a
Miller Mambuscay <millerrestivenmambuscayvargas@gmail.com>
2023-03-17 15:03:57 -0500

    Se agrego correctamente las tablas

--- Practica3.2.py
+++ Practica3.2.py
@@ -1,26 +1,31 @@
 from prettytable import PrettyTable
 
-factor_s=float(input("Ingrese factor de servicio entre 1 y 3: "))
-while factor_s<1 or factor_s>3:
-    factor_s=float(input("Datos incorrectos \nIngrese factor de servicio entre 1 y 3: "))      
+tabla_c = PrettyTable()
+tabla_a= PrettyTable()
+def init():
+    factor_s=float(input("Ingrese factor de servicio entre 1 y 3: "))
+    while factor_s<1 or factor_s>3:
+        factor_s=float(input("Datos incorrectos \nIngrese factor de servicio entre 1 y 3: "))      
 
-caudal=float(input("Ingrese caudal: "))
-temp_e=float(input("Ingrese temperatura de entrada: "))
-temp_s=float(input("Ingrese temperatura de salida "))
-                     
-const1=1000
-const2=0.0003069
-#********************
-caudal=774
-temp_e= 10
-temp_s=5.5
-factor_s=1.6
-#Resultado 1710 
-#********************
-tamañoDT=caudal*(temp_e-temp_s)*factor_s*const1*const2
-print("Tamaño del distrito termico es: ",tamañoDT)
+    caudal=float(input("Ingrese caudal: "))
+    temp_e=float(input("Ingrese temperatura de entrada: "))
+    temp_s=float(input("Ingrese temperatura de salida "))
+                        
+    const1=1000
+    const2=0.0003069
+    #********************
+    caudal=774
+    temp_e= 10
+    temp_s=5.5
+    factor_s=1.6
+    #Resultado 1710 
+    #********************
+    tamañoDT=int(caudal*(temp_e-temp_s)*factor_s*const1*const2)
+    print("Tamaño del distrito termico es: ",tamañoDT)
 
-def chiller():
+    chiller(tamañoDT)
+
+def chiller(tamañodt):
     print("\n Tamaños de chillers Centrífugos y de Absorción 500TR, 750TR, 1000TR \n")
     print("Favor indicar cantidad y lea con detenimiento \n")
     print("__________________________________________________________ \n")
@@ -36,7 +41,7 @@ def chiller():
     c500 = 2
     c750 = 0
     c1000 =0
-    aa500 = 1000
+    aa500 = 2
     a750 = 0
     a1000 = 0
 
@@ -46,22 +51,29 @@ def chiller():
     total_a=(500*aa500)+(750*a750)+(1000*a1000)
     total_ch=total_c+total_a
 
-    
-    tope_max=total_ch*(total_ch*0.5)
+    print("El total de las suma de chiller es: ",total_ch)
 
-    if(tamañoDT>total_ch):
+    tope_max=int(total_ch+(total_ch*0.5))
+
+    print("El tope maximo es: ",tope_max)
+
+    if(total_ch<=tamañodt):
         print("\n Las tecnologías seleccionadas no suministran el tamaño del DT \n")
         print("__________________________________________________________ \n")
-    elif(tamañoDT<tope_max):
+        chiller()
+    elif(total_ch>=tope_max):
         print("\n Las tecnologías seleccionadas superan el tope del DT")
         print("__________________________________________________________ \n") 
-        chiller()  
+        chiller()
+    else: 
+        print(total_c) 
         centrifugos(total_c)
         absorcion(total_a)     
 
-chiller()
+
 
 ###################
+
 
 def centrifugos(parametro1):
     # 1000
@@ -74,43 +86,39 @@ def centrifugos(parametro1):
     ft=capex*1000000 #3517411
     e=capex*1700000	#597_9
     b=capex*2000000 #7034822
+
+    tabla_c.field_names = ["Energía", "Emisiones", "Capex", "Opex"]
+
+    tabla_c.add_row(["Red publica", rp, 0, 0])
+    tabla_c.add_row(["Microturbina a gas", g, c, o])
+    tabla_c.add_row(["Solar fotovoltaica", 0, ft, 0])
+    tabla_c.add_row(["Energía eóilca", 0, e, 0])
+    tabla_c.add_row(["Energía biomasa", 0, b, 0])
+    
+
+    
     crearTablas('centri')
+
 
 def absorcion(parametro2):
     # 1000
     g=(parametro2*511.13199046407)/1000	# 51113199046	
     c=((parametro2 * 0.0035174111853)*(1925000/0.88)) #7.694.336,96784375	
     o=c*0.03 # 230.830		
-  		
+    
     capex=parametro2*0.0035174111853 # 3,5174111853 //No cuadra del todo	
     ft=(capex*1000000)*1.015 #1.015.000.000 //no cuadra		
-    b=capex*2000000 # 7.034.822,3706		
+    b=capex*2000000 # 7.034.822,3706
+
+    tabla_a.field_names = ["Energía", "Emisiones", "Capex", "Opex"]
+
+    tabla_a.add_row(["Microturbina a gas", g, c, o])
+    tabla_a.add_row(["Solar termica", 0, capex, 0])
+    tabla_a.add_row(["Energía biomasa", 0, b, 0])
+    
+
     crearTablas('abso')
-#*********************
 
-
-tabla_c = PrettyTable()
-tabla_a= PrettyTable()
-
-# Definir las columnas de la tabla
-tabla_c.field_names = ["Energía", "Emisiones", "Capex", "Opex"]
-tabla_a.field_names = ["Energía", "Emisiones", "Capex", "Opex"]
-
-# Agregar filas a la tabla
-tabla_c.add_row(["Red publica", 25, "Buenos Aires", "Buenos Aires"])
-tabla_c.add_row(["Microturbina a gas", 30, "Córdoba", "Buenos Aires"])
-tabla_c.add_row(["Solar fotovoltaica", 20, "Rosario", "Buenos Aires"])
-tabla_c.add_row(["Energía eóilca", 25, "Buenos Aires", "Buenos Aires"])
-tabla_c.add_row(["Energía biomasa", 30, "Córdoba", "Buenos Aires"])
-tabla_c.add_row(["Toneladas de refrigeracion", 30, "Córdoba", "Buenos Aires"])
-
-tabla_a.add_row(["Microturbina a gas", 30, "Córdoba", "Buenos Aires"])
-tabla_a.add_row(["Solar termica", 20, "Rosario", "Buenos Aires"])
-tabla_a.add_row(["Energía biomasa", 30, "Córdoba", "Buenos Aires"])
-tabla_a.add_row(["Toneladas de refrigeracion", 30, "Córdoba", "Buenos Aires"])
-
-
-#*************************
 
 def crearTablas(resp):
     if resp == 'centri':
@@ -118,5 +126,5 @@ def crearTablas(resp):
     elif resp == 'abso':
         print(tabla_a)	
 
-
+init()
     
